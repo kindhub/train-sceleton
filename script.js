9c545a60ca680254b9976d86dcaff12a574090b8
Султан Жекебаев <zhekebaevsm@gmail.com>
2023-04-28 21:54:43 +0600

    add promise.finally

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const carImage = document.querySelector('#car-image');
-const skeleton = document.querySelector('.car-image-skeleton');
-const btnCar = document.querySelector('#car-btn');
+const carImage = document.querySelector("#car-image");
+const skeleton = document.querySelector(".car-image-skeleton");
+const btnCar = document.querySelector("#car-btn");
 
 function getRandomImage() {
-  skeleton.style.display = 'block';
-  carImage.style.backgroundImage = 'none';
+  skeleton.style.display = "block";
+  carImage.style.backgroundImage = "none";
 
   axios
     .get("https://source.unsplash.com/random/600x400/?car")
     .then((response) => {
       carImage.style.backgroundImage = `url(${response.request.responseURL})`;
-      skeleton.style.display = 'none';
+      skeleton.style.display = "none";
     })
-    .catch(error => {
-        console.error(error);
-        const errorImage = document.querySelector('.error');
-        document.body.appendChild(errorImage);
-        errorImage.style.display = 'block';
-        skeleton.style.display = 'none';
-        btnCar.style.display = 'none';
-        carImage.style.display = 'none';
-      });
+    .catch((error) => {
+      console.error(error);
+      const errorImage = document.querySelector(".error");
+      document.body.appendChild(errorImage);
+      errorImage.style.display = "block";
+      skeleton.style.display = "none";
+      btnCar.style.display = "none";
+      carImage.style.display = "none";
+    })
+    .finally(() => {
+      const loader = document.querySelector(".loader");
+      loader.style.display = "none";
+    });
 }
 
-btnCar.addEventListener('click', getRandomImage);
+btnCar.addEventListener("click", getRandomImage);
 
 getRandomImage();
